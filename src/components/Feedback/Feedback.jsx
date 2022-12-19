689922dcd4184162f0ab4e934bfaa93733d209dc
Serge-Moskalenko <skmos2019@outlook.com>
2022-12-19 13:06:26 +0000

    hook

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,39 +1,25 @@
-import React, { Component } from "react";
+import {useState} from "react";
 import { Statistics } from "components/Statistics/Statistics";
 import { FeedbackButton } from "./FeedbackButton";
 
-export class Feedback extends Component {
-    
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-    };
-    
-    onClickGood = () => {this.setState(e => ({good: e.good + 1}))}
+export function Feedback() {
 
-    onClickNeutral = () => {this.setState(e => ({neutral: e.neutral + 1}))};
+    const [good, setGood ] = useState(0);
+    const [ neutral, setNeutral ] = useState(0);
+    const [ bad, setBad ] = useState(0);
 
-    onClickBad = () => { this.setState(e => ({ bad: e.bad + 1 })) };
-    
+    const total = 0 + good + neutral + bad;
 
-    render() {
+    const pesent = good / total * 100;
 
-        const { good, neutral, bad } = this.state
-        
-        const total = 0 + good + neutral + bad;
-
-        const pesent=good/total*100
-
-        return (
-            <div>
+    return( <div>
 
                 <h1>Please leave feedback</h1>
 
                 <FeedbackButton
-                    good={this.onClickGood}
-                    neutral={this.onClickNeutral}
-                    bad={this.onClickBad}
+                    good={()=>setGood(good + 1)}
+                    neutral={()=>setNeutral(neutral + 1)}
+                    bad={()=>setBad(bad + 1 )}
                 />
 
                 <h2>Statistics</h2>
@@ -46,7 +32,5 @@ export class Feedback extends Component {
                     total={total}
                     positivePercentage={good && pesent.toFixed()} />
                 :'Not feedback given'}
-            </div>
-        )
-    }
+            </div>)
 };
